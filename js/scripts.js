db93e34ce5645f9eb6598d19c91240bac87ceb50
Austin Barr, Curt Caldwell, Eddie Harris and Nick Rogers <nick@>
2018-06-18 10:35:02 -0700

    add first dating profile

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
-var person = function(name, age, aboutMe, astrolgical, occupation) {
+var Person = function(name, age, aboutMe, astrolgical, occupation) {
   this.name = name;
   this.age = age;
   this.aboutMe; aboutMe;
   this.astrolgical = astrolgical;
   this.occupation = occupation;
+  this.dark = dark;
 }
+
+var profileNames = ["Helga", "Mindy", "Jane "];
+var profileAges = [45];
+var profileAboutMes = [
+                        "My name is " + profileNames[Math.floor((Math.random() * 3))] + " I hail from Bavaria. I like beer, bratwurst and Oompah music.  I hold the record for Das Boot in all of Germany. I like strong aggressive men.",
+                      ];
+
+                      
+var profileAstrologicals = ["Scorpio"];
+var profileOccupations = ["Beer Witch"];
+
+$(document).ready(function(){
+
+
+
+
+
+
+document.getElementById("test").innerHTML = profileAboutMes;
+
+
+});
