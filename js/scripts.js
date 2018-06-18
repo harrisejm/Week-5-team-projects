f75af7f6dbbac09e2717c5fbdca732a65a9f2d2c
Austin Barr, Curt Caldwell, Eddie Harris and Nick Rogers <austin@>
2018-06-18 09:23:37 -0700

    add initial project folder

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var person = function(name, age, aboutMe, astrolgical, occupation) {
+  this.name = name;
+  this.age = age;
+  this.aboutMe; aboutMe;
+  this.astrolgical = astrolgical;
+  this.occupation = occupation;
+}
